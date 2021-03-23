6012089e264ff5b2530ea62a3b94696aa58fa1d5
ynohtn <anthonyrnd69@gmail.com>
2021-03-23 17:15:33 +0100

    feat(page): add Page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { products } from '~/data'
 import ProductGrid from '~/components/ProductGrid'
-import Cart from '~/components/Cart'
 
 export default function Home() {
   return (
     <div className="home">
       <ProductGrid products={products} />
-      <Cart />
     </div>
   )
 }
