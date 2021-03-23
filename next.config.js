6ea7f67478e01dc6b1162a72ec0c33a2ec9e1dee
ynohtn <anthonyrnd69@gmail.com>
2021-03-23 14:42:10 +0100

    chore(all) : initial commit

---
+++ next.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  sassOptions: {
+    includePaths: ['src'],
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+  },
+  webpack(config) {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: { ref: true },
+          },
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/_next/static/images/',
+              outputPath: 'static/images/',
+            },
+          },
+        ],
+      },
+    ]
+    return config
+  },
+}
