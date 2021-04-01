ad969486d3f7c6569555452583e29d22d58ec127
ynohtn <anthonyrnd69@gmail.com>
2021-04-01 15:47:54 +0200

    feat(nav): add burger

---
+++ src/components/Header/Nav/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import classNames from 'classnames/bind';
+import { useWindowSize } from 'react-use';
+import { DropdownType, nav } from '~/data';
+import css from './styles.module.scss';
+import Dropdown from '../Dropdown';
+
+const cx = classNames.bind(css);
+
+export interface NavProps {
+  className?: string;
+}
+
+function Nav({ className }: NavProps) {
+  const { width } = useWindowSize();
+  const isMobile = (width <= 768);
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  return (
+    <div className={cx(className, css.Nav)}>
+      <button className={css.burger} onClick={() => setIsOpen(!isOpen)}>
+        <div className={cx(css.icon, isOpen ? 'open' : '')}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+      <nav className={cx(className, isMobile ? css.mobileNav : css.desktopNav, { isOpen })}>
+        <ul className={css.items}>
+          {nav.map((dropdown: DropdownType) => <Dropdown key={dropdown.text} dropdown={dropdown} />)}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+Nav.defaultProps = {};
+
+export default Nav;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,7 @@
 import classNames from 'classnames/bind';
-import { nav, DropdownType } from '~/data'
 import css from './styles.module.scss';
 import CartToggler from '../CartToggler';
-import Dropdown from './Dropdown';
+import Nav from './Nav'
 
 const cx = classNames.bind(css);
 
@@ -15,17 +14,12 @@ const logo: string = '/logo.png'
 function Header({ className }: HeaderProps) {
   return (
     <header className={cx(className, css.Header)}>
-      <nav className={cx(className, css.navLeft)}>
-        <ul>
-          {nav.map((dropdown: DropdownType) => <Dropdown key={dropdown.text} dropdown={dropdown} />)}
-        </ul>
-      </nav>
+      <Nav />
       <figure className={css.logo}>
         <img src={logo} alt="TODO" />
       </figure>
       <nav className={css.navRight}>
         <ul>
-          <li>Account</li>
           <li>
             <CartToggler />
           </li>
